8b10d60227b2de7b4cfea79d3a75ead90d84f024
Богдан Колганов <kolganov_25@mail.ru>
2024-10-23 02:55:13 +0700

    -fix

--- Sem3_Alaba1/ComputerDevice.cs
+++ Sem3_Alaba1/ComputerDevice.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sem3_Alaba1
+﻿namespace Sem3_Alaba1
 {
     public class ComputerDevice
     {
--- Sem3_Alaba1/ComputerDeviceFactory.cs
+++ Sem3_Alaba1/ComputerDeviceFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sem3_Alaba1
+﻿namespace Sem3_Alaba1
 {
     public static class ComputerDeviceFactory
     {
